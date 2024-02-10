a7cdb8b352eaa0fd1b4b1464392e27bd37828d3b
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-02-10 19:46:54 +0300

    Add Intuition paragraph

--- 2_Add_Two_Numbers/2_Add_two_numbers.playground/Contents.swift
+++ 2_Add_Two_Numbers/2_Add_two_numbers.playground/Contents.swift
@@ -32,13 +32,36 @@ class ListNode {
 
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        return ListNode()
+        let dummyHead = ListNode(0)
+        var curr = dummyHead
+        var carry = 0, l1 = l1, l2 = l2
+        
+        while l1 != nil || l2 != nil || carry != 0 {
+            let x = l1?.value ?? 0
+            let y = l2?.value ?? 0
+            let sum = carry + x + y
+            carry = sum / 10
+            curr.next = ListNode(sum % 10)
+            curr = curr.next!
+            l1 = l1?.next
+            l2 = l2?.next
+        }
+        
+        return dummyHead.next
     }
 }
 
-let list1 = ListNode()
-let list2 = ListNode()
+let forthNodeOfTheFirstList = ListNode(12, nil)
+let thirdNodeOfTheFirstList = ListNode(9, forthNodeOfTheFirstList)
+let secondNodeOfTheFirstList = ListNode(6, thirdNodeOfTheFirstList)
+let firstNodeOfTheFirstList = ListNode(1, secondNodeOfTheFirstList)
 
-let example = Solution()
-example.addTwoNumbers(list1, list2)
+let thirdNodeOfTheSecondList = ListNode(8, nil)
+let secondNodeOfTheSecondList = ListNode(5, thirdNodeOfTheSecondList)
+let firstNodeOfTheSecondList = ListNode(2, secondNodeOfTheSecondList)
+
+let solution = Solution()
+let unionList = solution.addTwoNumbers(firstNodeOfTheFirstList, firstNodeOfTheSecondList)
+
+print(unionList!)
 
