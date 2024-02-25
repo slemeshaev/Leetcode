38f525225edeac0fc025f13dd728c6551b01ed6b
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-02-25 20:21:21 +0300

    Condition for third task

---
+++ 3_Longest_Substring/3_Longest_substring.playground/Contents.swift
@@ -0,0 +1,4 @@
+// Задача 3: Самая длинная подстрока без повторяющихся символов (Longest substring without repeating characters)
+
+// Описание и решение задачи находятся в файле 3_Longest_substring.md
+
